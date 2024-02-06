ce7339ec132b9ab554adc55cbb6315e56cf8cf7f
Phillip <phillip@vms-MacBook-Pro.local>
2024-02-06 14:06:55 +0300

    updates Dockerfile to build the consumer binary via a 2 stage

--- consumer/build.sh
+++ consumer/build.sh
@@ -5,5 +5,5 @@ export GOARCH=amd64
 go mod tidy
 go build -o consumer .
 
-docker buildx build --platform=linux/amd64 . -t rtukpe/benchmarks-consumer:v8
-docker push  rtukpe/benchmarks-consumer:v8
+docker buildx build --platform=linux/amd64 . -t rtukpe/benchmarks-consumer:v9
+docker push  rtukpe/benchmarks-consumer:v9
